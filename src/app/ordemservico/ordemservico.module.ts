abfc0a7f205bc481df22d8cb7f6cf1dcae6f1343
Fernando S Medeiros <fernando.medeiros.ti@gmail.com>
2018-04-23 19:40:04 -0300

    componente para cadastro de OS

--- src/app/ordemservico/ordemservico.module.ts
+++ src/app/ordemservico/ordemservico.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrdemServicoComponent } from './ordemservico.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    OrdemServicoComponent
+    OrdemServicoComponent,
+    CadastroComponent
   ],
   exports: [
-    OrdemServicoComponent
+    OrdemServicoComponent,
+    CadastroComponent
   ]
 })
 export class OrdemservicoModule { }
