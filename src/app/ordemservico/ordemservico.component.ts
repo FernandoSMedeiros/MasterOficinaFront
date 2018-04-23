94c60b7638b5f433eb8e83779134d33138f04849
Fernando S Medeiros <fernando.medeiros.ti@gmail.com>
2018-04-22 21:39:28 -0300

    first commit ordemservico component

---
+++ src/app/ordemservico/ordemservico.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ordemservico',
+  templateUrl: './ordemservico.component.html',
+  styleUrls: ['./ordemservico.component.css']
+})
+export class OrdemServicoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/ordemservico/ordemservico.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OrdemServicoComponent } from './ordemservico.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    OrdemServicoComponent
+  ],
+  exports: [
+    OrdemServicoComponent
+  ]
+})
+export class OrdemservicoModule { }
