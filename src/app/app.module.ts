ba048d7012b1cd41aea12ee01a2d2844977d1ad5
Fernando S Medeiros <fernando.medeiros.ti@gmail.com>
2018-05-01 15:21:25 -0300

    Tabela para listagem de OS
     Adição de comportamento de click duplo na linha da tabela.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { OrdemservicoModule } from './ordemservico/ordemservico.module';
-import { ListaComponent } from './lista/lista.component';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ListaComponent
+    AppComponent    
   ],
   imports: [
     BrowserModule,
