a6d90264ff4c0fef7996bf9b410232f6fca16ca9
Fernando S Medeiros <fernando.medeiros.ti@gmail.com>
2018-05-20 09:11:38 -0300

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { OrdemservicoModule } from './ordemservico/ordemservico.module';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent    
+    AppComponent,
+    NavbarComponent    
   ],
   imports: [
     BrowserModule,
